a02052b1214c61e29d0615341d303c20af4326fa
Tano <tanoni44@gmail.com>
2024-08-31 18:53:04 -0300

    first commit

---
+++ functions/intro.py
@@ -0,0 +1,12 @@
+print('*** Funciones en Python ***')
+
+# Funcion para ,mandar un saludo
+
+def greet():
+   name = input('Ingrese su nombre: ')
+   print(f'Buenas tardes {name}, espero que te encuentres bien.')
+
+
+# Llamamos la funcion, en python las mismas con objetos
+greet()
+
